7ab0997194abc7be59d54e80972e80432a645db0
radudotat <gith@radu.at>
2022-05-08 22:41:39 +0200

    Add project to GitHub

--- key-store.js
+++ key-store.js
@@ -1,11 +1,33 @@
 const VALID_KEYS_PATH = __dirname + '/valid-keys.txt';
 const fs = require('fs');
 const shortid = require('shortid');
+const { fileToSet } = require('./lib/util');
 // To generate a unique API KEY, use shortid.generate()
 const LINE_ENDING = require('os').EOL;
 
-
 module.exports = function (req, res) {
+  let newKey = shortid.generate();
+  (async () => {
+    // const result = await fs.promises.readFile(VALID_KEYS_PATH);
+    // const arrayOfLines = result.toString().trim().split(LINE_ENDING);
+    const lines = await fileToSet(VALID_KEYS_PATH);
 
+    // loop until the generated key is unique
+    while (lines.has(newKey) === true) {
+      newKey = shortid.generate();
+    }
+
+    await fs.promises.appendFile(
+      VALID_KEYS_PATH,
+      `${newKey}${LINE_ENDING}`,
+      (error) => {
+        if (error) {
+          console.error(error);
+          res.status(500).send(error);
+        }
+      },
+    );
+
+    res.status(201).json({ apiKey: newKey });
+  })();
 };
-
--- middleware.js
+++ middleware.js
@@ -1,5 +1,18 @@
+const { fileToSet } = require('./lib/util');
+
 const VALID_KEYS_PATH = __dirname + '/valid-keys.txt';
 
 module.exports = function (req, res, next) {
+  (async () => {
+    const apiKeys = await fileToSet(VALID_KEYS_PATH);
+    const requestKey = req.get('x-api-key')?.trim();
+    if (requestKey !== 'undefined' || requestKey !== '') {
+      if (apiKeys.has(requestKey)) {
+        return next();
+      }
+    }
 
+    // console.log('apiKeys', apiKeys, requestKey);
+    res.status(401).json({ error: 'Invalid API Key' });
+  })();
 };
